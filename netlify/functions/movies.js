d903d10757b5a833e83a199b9ec01f27d6c5bb04
Your Name <anant.vijay@kellogg.northwestern.edu>
2021-05-07 18:24:34 -0400

    finalcommit

--- netlify/functions/movies.js
+++ netlify/functions/movies.js
@@ -18,30 +18,56 @@ exports.handler = async function(event) {
   // write the movies to the back-end console, check it out
   console.log(moviesFromCsv)
 
+
   // 🔥 hw6: your recipe and code starts here!
+  // Get the year and genre from the querystring parameters
   let year = event.queryStringParameters.year
   let genre = event.queryStringParameters.genre
-  
+
+  // See if any of the above variables result in undefined and if so, return Nope! 
   if (year == undefined || genre == undefined) {
     return {
       statusCode: 200, // https://developer.mozilla.org/en-US/docs/Web/HTTP/Status
       body: `Nope!` // a string of data
     }
   }
+  
+  // When conditions have been properly met, run this code
   else {
+  
+  // Create the object returnValue which stores the count and the movie array
     let returnValue = {
       numResults: 0,
       movies: []
     }
-
+  
+  // Loop through all listings   
     for (let i=0; i < moviesFromCsv.length; i++) {
-
-    }
-
-    // a lambda function returns a status code and a string of data
-    return {
-      statusCode: 200, // https://developer.mozilla.org/en-US/docs/Web/HTTP/Status
-      body: `Hello from the back-end!` // a string of data
+  
+  // Store each listing in memory
+      let movieResults = moviesFromCsv[i]
+      
+  // Create a new post object containing the pertinent fields    
+      let moviePost = {
+        Title: movieResults.primaryTitle,
+        Release_Date: movieResults.startYear,
+        Genres: movieResults.genres
+      }
+  
+  // Check if it meets the criteria input by the user and also ask it to ignore any //N runtime and genres
+      if(movieResults.genres == genre && movieResults.startYear == year && movieResults.runtimeMinutes !== `\\N` && movieResults.genres !== `\\N`){
+      
+  // Add the results to the array    
+      returnValue.movies.push(moviePost)
+      
+  // Increase the counter by 1    
+      returnValue.numResults=returnValue.numResults+1  
     }
   }
+    // a lambda function returns a status code and a string of data
+  return {
+    statusCode: 200, // https://developer.mozilla.org/en-US/docs/Web/HTTP/Status
+    body: JSON.stringify(returnValue) // a string of data
+  }
+}
 }
